7004b12b96c93329ef269f83f0c3d51c59ef8d9a
lordkleiton <m_s_r@outlook.com>
2018-09-29 01:18:35 -0300

    agora ele ja converte de txt pra json

--- txt-json.js
+++ txt-json.js
@@ -2,13 +2,14 @@
 
 Código para a abertura de txt => https://stackoverflow.com/a/46801928
 
+o método pra abrir o txt é bem desnecessariamente complicado, mas nao tava com vontade
+de criar o meu próprio
+
 essa aplicação pega um arquivo txt simples, onde cada palavra fica em uma linha
 e transforma em um json.
 
 os arquivos devem se chamar, respectivamente: positivo.txt e negativo.txt;
 ambos devem se localizar na pasta dicionarios
-
-por enquanto ele não faz nada, só lê o arquivo e imprime no console
 
 */
 
@@ -19,5 +20,19 @@ let data = ''
 readStream.on('data', function(chunk) {
     data += chunk;
 }).on('end', function() {
-    console.log(data.split("\n").length);
+    dados = data.split("\n");
+
+    var palavras = {
+        "palavras":[]
+    };
+
+    
+    for (var i = 0; i < dados.length; i++){
+        palavras.palavras.push({"palavra": dados[i]});
+    }
+
+    console.log(palavras);
+    fs.writeFile('./dicionarios/teste.json', JSON.stringify(palavras, null, 4), (err) => {  
+        if (err) throw err;
+    });
 });
