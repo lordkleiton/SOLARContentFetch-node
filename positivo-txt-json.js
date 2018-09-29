9f8a037e1226dad9c877fc552433e3c64e02317c
lordkleiton <m_s_r@outlook.com>
2018-09-29 01:59:18 -0300

    os conversores ja funcionam, mas ainda nao sao um unico arquivo

---
+++ positivo-txt-json.js
@@ -0,0 +1,50 @@
+/*
+
+Código para a abertura de txt => https://stackoverflow.com/a/46801928
+
+o método pra abrir o txt é bem desnecessariamente complicado, mas nao tava com vontade
+de criar o meu próprio
+
+essa aplicação pega um arquivo txt simples, onde cada palavra fica em uma linha
+e transforma em um json.
+
+o arquivo deve se chamar positivo.txt e deve se localizar na pasta dicionarios
+
+os dois arquivos que convertem deveriam ser os mesmos, mas aconteceram problemas 
+que não permitiram, entao ate ou eu ter tempo ou ter outra ideia, ficam separados mesmo
+
+*/
+
+var path = require('path');
+let data = '';
+
+convertePositivo();
+
+function convertePositivo(){
+
+    var fs = require('fs');
+    var arquivo = fs.createReadStream(path.join(__dirname, './dicionarios') + '/positivo.txt', 'utf8');
+    
+    arquivo.on('data', function(chunk) {
+        data += chunk;
+    }).on('end', function() {
+
+        var dados = data.split("\n");
+
+        var palavras = {
+            "palavras":[]
+        };
+
+        
+        for (var i = 0; i < dados.length; i++){
+            palavras.palavras.push({"palavra": dados[i]});
+        }
+
+        console.log(palavras);
+        fs.writeFile('./dicionarios/positivo.json', JSON.stringify(palavras, null, 4), (err) => {  
+            if (err) throw err;
+        });
+    });
+
+}
+
