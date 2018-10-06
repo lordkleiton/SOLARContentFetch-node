b71464cf1fb4490e991795cca78ac7dbd0028c8c
lordkleiton <m_s_r@outlook.com>
2018-10-06 00:31:51 -0300

    ainda em testes

--- testes.js
+++ testes.js
@@ -63,31 +63,33 @@ for (var i = 0; i < tamanhoTextos; i++){
 }
 
 
+
 //ensina ao algoritmo as palavras negativas e positivas
 classifier.learn(dicionarioN.toString(), 'negative');
 classifier.learn(dicionarioP.toString(), 'positive');
 
+//caso queira testar com um texto qualquer descomente a linha abaixo e comente a outra
+var tt = 'Para odiar alguém gastamos energia, neurônios e tempo.';
+//var tt = todosTextos[40];
+
+
+//essa parte é pra quando tudo ja funcionar direitinho
 /*
-//categoriza a mensagem
-console.log(classifier.categorize('Heráclito(Devir) e Parmênides(Ser) nos trasmitem conceitos distintos, uma vez que para Heráclito o devir é a realidade, a harmonia quando se tem os opostos em equilíbrio daí origina-se o Ser. Parmênides defende a ideia de que o Ser, é a única realidade existente pelo princípio não contradição. Entre todas as divergências existentes é notório perceber a influência destes pensamentos no mundo atual e qual a percepção que temos dos mesmos. Somos seres mutáveis há fatos a se pensar!'));
-console.log(classifier.categorize('que vontade de matar'));
+for (var i = 0; i < tamanhoTextos; i++){
+    console.log(todosTextos[i]);
+    console.log("\n", classifier.categorize(todosTextos[i]));
+}
 
-
-// serialize the classifier's state as a JSON string.
-var stateJson = classifier.toJson()
-
-// load the classifier back from its JSON representation.
-var revivedClassifier = bayes.fromJson(stateJson)
+for (var i = 0; i < tamanhoTextos; i++){
+    tt = sw.removeStopwords(todosTextos[i].split(' '), dicionarioS).toString().replace(/,/g, ' ');
+    console.log(tt);
+    console.log("\n", classifier.categorize(tt));
+}
 */
-//console.log("negativas: ", tamanhoN);
-//console.log("positivas: ", tamanhoP);
-
-var tt = 'Você não ouviu o que eu disse?';
-
-//console.log(tt);
-
-tt = sw.removeStopwords(tt.split(' '), dicionarioS).toString().replace(/,/g, ' ');
 
 console.log(tt);
+console.log("\n", classifier.categorize(tt));
 
+tt = sw.removeStopwords(tt.split(' '), dicionarioS).toString().replace(/,/g, ' ');
+console.log(tt);
 console.log("\n", classifier.categorize(tt));
